bd1c05ce129ed2caf2df1185a814963989eaa5a9
kparasch <konstantinos.paraschou@cern.ch>
2022-08-07 12:53:11 +0200

    create file if it doesn't exist

--- 000_update_dataframes.py
+++ 000_update_dataframes.py
@@ -1,8 +1,13 @@
 from ecloud_dataframes import EcloudDataframes, logger
 import pickle
+import os
 
+filename = "LHC_ecloud_dataframes_2022.pkl"
+if os.path.exists(filename):
+    ecloud_df = EcloudDataframes(dataframe_pickle=filename)
+else:
+    ecloud_df = EcloudDataframes(dataframe_pickle=None)
 
-ecloud_df = EcloudDataframes(dataframe_pickle="LHC_ecloud_dataframes_2022.pkl")
 ecloud_df.update()
 logger.info("Finished")
 
